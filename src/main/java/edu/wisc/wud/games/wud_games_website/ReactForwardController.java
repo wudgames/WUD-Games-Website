229c71927ee9f64984f871398e3de578492fbcfc
Wyatt <201182463+AgdG2K52UgjGLkj3VMgO@users.noreply.github.com>
2026-08-17 07:08:29 -0500

    Working dynamic value from model attribute

--- src/main/java/edu/wisc/wud/games/wud_games_website/ReactForwardController.java
+++ src/main/java/edu/wisc/wud/games/wud_games_website/ReactForwardController.java
@@ -1,8 +1,12 @@
 package edu.wisc.wud.games.wud_games_website;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import edu.wisc.wud.games.wud_games_website.user_account.UserAccountResource;
 import jakarta.servlet.http.HttpServletRequest;
 
 
@@ -12,11 +16,15 @@ import jakarta.servlet.http.HttpServletRequest;
 @Controller
 public class ReactForwardController {
 
+    @Autowired
+    UserAccountResource userAccountResource;
+
     @GetMapping("{path:^(?!api|public|css|js|images)[^\\.]*}/**")
-    public String handleForward(HttpServletRequest request) {
+    public String handleForward(HttpServletRequest request, Model model) {
         String uri = request.getRequestURI();
         // This is where we can add more logic to determine which path should be forwarded. For example, you could check the URI and forward it to a specific controller or view.
         if (uri.startsWith("/myuser")) {
+            model.addAttribute("user_email", "example hard coded value");
             return "myuser";
         }
         
