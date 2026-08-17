fc024587e6bfa9b2e74b47f0ba8ddc35a2d53310
Wyatt <201182463+AgdG2K52UgjGLkj3VMgO@users.noreply.github.com>
2026-08-17 08:10:10 -0500

    General cleanup

--- src/main/java/edu/wisc/wud/games/wud_games_website/user_account/UserAccountResource.java
+++ src/main/java/edu/wisc/wud/games/wud_games_website/user_account/UserAccountResource.java
@@ -1,19 +1,15 @@
 package edu.wisc.wud.games.wud_games_website.user_account;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
