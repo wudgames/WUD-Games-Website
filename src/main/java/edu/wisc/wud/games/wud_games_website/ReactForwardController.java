6b0df4ed1cc1cb4d00257bf3999e9d8d7842766f
Wyatt <201182463+AgdG2K52UgjGLkj3VMgO@users.noreply.github.com>
2026-08-17 06:54:28 -0500

    Work navigation to myuser page

--- src/main/java/edu/wisc/wud/games/wud_games_website/ReactForwardController.java
+++ src/main/java/edu/wisc/wud/games/wud_games_website/ReactForwardController.java
@@ -3,6 +3,8 @@ package edu.wisc.wud.games.wud_games_website;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 
 /**
  * Serve Reacts index.html for all requests that are not relevant for the backend.
@@ -11,7 +13,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ReactForwardController {
 
     @GetMapping("{path:^(?!api|public|css|js|images)[^\\.]*}/**")
-    public String handleForward() {
+    public String handleForward(HttpServletRequest request) {
+        String uri = request.getRequestURI();
+        // This is where we can add more logic to determine which path should be forwarded. For example, you could check the URI and forward it to a specific controller or view.
+        if (uri.startsWith("/myuser")) {
+            return "myuser";
+        }
+        
         return "forward:/";
     }
 
