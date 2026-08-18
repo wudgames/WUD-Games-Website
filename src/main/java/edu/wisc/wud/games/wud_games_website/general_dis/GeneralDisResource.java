ad24c9364c48fbfceb52cfd3dbb714e6849feb01
Wyatt <201182463+AgdG2K52UgjGLkj3VMgO@users.noreply.github.com>
2026-08-18 05:49:32 -0500

    queryParameters working with htmx

--- src/main/java/edu/wisc/wud/games/wud_games_website/general_dis/GeneralDisResource.java
+++ src/main/java/edu/wisc/wud/games/wud_games_website/general_dis/GeneralDisResource.java
@@ -2,6 +2,8 @@ package edu.wisc.wud.games.wud_games_website.general_dis;
 
 import jakarta.validation.Valid;
 import java.util.List;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
@@ -11,12 +13,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.bind.annotation.RequestParam;
-
-
-
 
 @RestController
 public class GeneralDisResource {
@@ -28,17 +27,22 @@ public class GeneralDisResource {
     }
     
     @GetMapping("/library")
-    public ModelAndView librarySearch(Model model) {
-        List<GeneralDisDTO> generalDisDTOList = generalDisService.findAll();
-        model.addAttribute("generalDisDTOList", generalDisDTOList);
-        System.out.println(generalDisDTOList);
+    public ModelAndView librarySearch(Model model, @RequestParam Map<String, String> queryParameters) {
+        attachResults(model, queryParameters);
         return new ModelAndView("search/generaldis");
     }
 
     /* This is called to refresh the the html for displaying a search result */
     @GetMapping("/api/search")
-    public ModelAndView getMethodName() {
+    public ModelAndView getMethodNameM(Model model, @RequestParam Map<String, String> queryParameters) {
+        attachResults(model, queryParameters);
         return new ModelAndView("search/result");
+    }
+
+    private void attachResults(Model model, Map<String, String> queryParameters) {
+        System.out.println("this could do something based on: " + queryParameters.get("searchterm"));
+        List<GeneralDisDTO> generalDisDTOList = generalDisService.findAll();
+        model.addAttribute("generalDisDTOList", generalDisDTOList);
     }
 
     @GetMapping("/api/generalDiss")
