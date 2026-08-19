d76aad3b3a197e91185056af0957ec96a86f62cd
Wyatt <201182463+AgdG2K52UgjGLkj3VMgO@users.noreply.github.com>
2026-08-19 15:59:36 -0500

    header is copied from template

--- src/main/java/edu/wisc/wud/games/wud_games_website/physical_item/PhysicalItemResource.java
+++ src/main/java/edu/wisc/wud/games/wud_games_website/physical_item/PhysicalItemResource.java
@@ -19,7 +19,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 
 @RestController
-@RequestMapping(value = "/api/physicalItems", produces = MediaType.APPLICATION_JSON_VALUE)
 public class PhysicalItemResource {
 
     private final PhysicalItemService physicalItemService;
@@ -30,36 +29,36 @@ public class PhysicalItemResource {
 
     @PreAuthorize("hasRole('PHYSICAL_INVENTORY_MANAGER')")
     @GetMapping("/manage/inventory/physicalItems")
-    public ModelAndView getMethodName(@RequestParam String param) {
+    public ModelAndView getMethodName() {
         return new ModelAndView("/manage/inventory/physicalItems");
     }
 
-    @GetMapping
+    @GetMapping("/api/physicalItems")
     public ResponseEntity<List<PhysicalItemDTO>> getAllPhysicalItems() {
         return ResponseEntity.ok(physicalItemService.findAll());
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/api/physicalItems/{id}")
     public ResponseEntity<PhysicalItemDTO> getPhysicalItem(
             @PathVariable(name = "id") final Long id) {
         return ResponseEntity.ok(physicalItemService.get(id));
     }
 
-    @PostMapping
+    @PostMapping("/api/physicalItems")
     public ResponseEntity<Long> createPhysicalItem(
             @RequestBody @Valid final PhysicalItemDTO physicalItemDTO) {
         final Long createdId = physicalItemService.create(physicalItemDTO);
         return new ResponseEntity<>(createdId, HttpStatus.CREATED);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/api/physicalItems/{id}")
     public ResponseEntity<Long> updatePhysicalItem(@PathVariable(name = "id") final Long id,
             @RequestBody @Valid final PhysicalItemDTO physicalItemDTO) {
         physicalItemService.update(id, physicalItemDTO);
         return ResponseEntity.ok(id);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/api/physicalItems/{id}")
     public ResponseEntity<Void> deletePhysicalItem(@PathVariable(name = "id") final Long id) {
         physicalItemService.delete(id);
         return ResponseEntity.noContent().build();
