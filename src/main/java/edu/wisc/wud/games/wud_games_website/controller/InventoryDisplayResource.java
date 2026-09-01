89ea1d98a6c491cf4b3b0d1f6f4da18ffe7def1d
Wyatt <201182463+AgdG2K52UgjGLkj3VMgO@users.noreply.github.com>
2026-09-01 09:35:32 -0500

    getCommonAncestor working

--- src/main/java/edu/wisc/wud/games/wud_games_website/controller/InventoryDisplayResource.java
+++ src/main/java/edu/wisc/wud/games/wud_games_website/controller/InventoryDisplayResource.java
@@ -1,7 +1,15 @@
 package edu.wisc.wud.games.wud_games_website.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.function.Consumer;
+
+import javax.management.RuntimeErrorException;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import edu.wisc.wud.games.wud_games_website.board_game_dis.BoardGameDisDTO;
+import edu.wisc.wud.games.wud_games_website.board_game_expansion_dis.BoardGameExpansionDisDTO;
 import edu.wisc.wud.games.wud_games_website.general_dis.GeneralDisDTO;
 import edu.wisc.wud.games.wud_games_website.general_dis.GeneralDisService;
 
@@ -16,8 +26,15 @@ import edu.wisc.wud.games.wud_games_website.general_dis.GeneralDisService;
 public class InventoryDisplayResource {
     private final GeneralDisService generalDisService;
 
+    // Used to pick what filter options to show and what service will handle the search
+    private final Map<Class<? extends GeneralDisDTO>, Consumer<Map<String, String>>> serviceBySharedAncestor = new HashMap<>();
+    private final Map<String, Class<? extends GeneralDisDTO>> classByDisplayString = new HashMap<>();
+
     public InventoryDisplayResource(GeneralDisService generalDisService) {
         this.generalDisService = generalDisService;
+
+        classByDisplayString.put("Board Game", BoardGameDisDTO.class);
+        classByDisplayString.put("Board Game Expansion", BoardGameExpansionDisDTO.class);
     }
 
     @GetMapping("/library")
@@ -41,6 +58,66 @@ public class InventoryDisplayResource {
         return getPageForDescription(id);
     }
 
+    private static Set<Class<?>> getClassesBfs(Class<?> clazz) {
+        Set<Class<?>> classes = new LinkedHashSet<Class<?>>();
+        Set<Class<?>> nextLevel = new LinkedHashSet<Class<?>>();
+        nextLevel.add(clazz);
+        do {
+            classes.addAll(nextLevel);
+            Set<Class<?>> thisLevel = new LinkedHashSet<Class<?>>(nextLevel);
+            nextLevel.clear();
+            for (Class<?> each : thisLevel) {
+                Class<?> superClass = each.getSuperclass();
+                if (superClass != null && superClass != Object.class) {
+                    nextLevel.add(superClass);
+                }
+                for (Class<?> eachInt : each.getInterfaces()) {
+                    nextLevel.add(eachInt);
+                }
+            }
+        } while (!nextLevel.isEmpty());
+        return classes;
+    }
+
+    private static List<Class<?>> commonSuperClass(Class<?>... classes) {
+        // start off with set from first hierarchy
+        Set<Class<?>> rollingIntersect = new LinkedHashSet<Class<?>>(
+                getClassesBfs(classes[0]));
+        // intersect with next
+        for (int i = 1; i < classes.length; i++) {
+            rollingIntersect.retainAll(getClassesBfs(classes[i]));
+        }
+        return new LinkedList<Class<?>>(rollingIntersect);
+    }
+
+    private Class<? extends GeneralDisDTO> getClassFor(String displayName) {
+        return classByDisplayString.get(displayName);
+    }
+
+    private Class<? extends GeneralDisDTO> getCommonAncestor(List<String> descriptionTypes) {
+        if (descriptionTypes.size() == 0) {
+            return GeneralDisDTO.class;
+        }
+        List<?> test = descriptionTypes.stream().map(this::getClassFor).toList();
+        Class<?>[] classArray = test.toArray(new Class<?>[0]);
+        Class<? extends GeneralDisDTO> commonAncestor = (Class<? extends GeneralDisDTO>) commonSuperClass(classArray).get(0);
+        return commonAncestor;
+    }
+
+    @GetMapping("/library/filterOptions")
+    public ModelAndView getMethodName(@RequestParam(name = "type") List<String> descriptionTypes) {
+        ModelAndView model = new ModelAndView("search/filterOptions");
+        // Identify common ancestor 
+        try {
+            Class<? extends GeneralDisDTO> commonAncestor = getCommonAncestor(descriptionTypes);
+            model.addObject("commonAncestor", commonAncestor);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        return model;
+    }
+
     /* This is called to refresh the the html for displaying a search result */
     @GetMapping("/api/search")
     public ModelAndView getSearchResult(@RequestParam Map<String, String> queryParameters) {
@@ -50,7 +127,8 @@ public class InventoryDisplayResource {
     }
 
     private ModelAndView attachResults(ModelAndView model, Map<String, String> queryParameters) {
-        System.out.println("this could do something based on: " + queryParameters.get("searchterm"));
+        //System.out.println("this could do something based on: " + queryParameters.get("searchterm"));
+        // TODO identity the service that will 
         try {
             return generalDisService.getResultsFor(model, queryParameters.get("searchterm"));
         } catch (Exception e) {
